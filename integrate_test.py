Fixed scaling of DOS.

integrate_test.py
@@ -24,7 +24,7 @@ Compute Integrals on a Training Dataset
 run = 'run11'
 label = 'target'
 
-with open('../' + run + '/temp/multi_out_predictions.json') as json_file:
+with open('../../' + run + '/temp/multi_out_predictions.json') as json_file:
     dos_dict = json.load(json_file)
 
 dft_3d = jdata("edos_pdos")
@@ -48,8 +48,8 @@ for i in dos_dict:
     target = np.array(i['target'])
     freq = np.linspace(0, 1000, len(target))
     int_DOS.append(pint.integrate_dos(freq, target))
-    S_vib.append(pint.vibrational_entropy(freq, target, 1))
-    Cp.append(pint.heat_capacity(freq, target, 1))
+    S_vib.append(pint.vibrational_entropy(freq, target))
+    Cp.append(pint.heat_capacity(freq, target))
 
 true_output = {'JID' : jid_list,
           'integrated_DOS' : int_DOS,
@@ -57,7 +57,7 @@ true_output = {'JID' : jid_list,
           'Cp (J/mol/K)' : Cp}
 df = pd.DataFrame(true_output)
 
-df.to_csv(run + label + '_thermal_props.csv')
+df.to_csv(run + label + '_thermal_props_scale_2.csv')
 
 
 
@@ -69,7 +69,7 @@ run = 'run11'
 
 label = 'predictions'
 
-with open('../' + run + '/temp/multi_out_predictions.json') as json_file:
+with open('../../' + run + '/temp/multi_out_predictions.json') as json_file:
     dos_dict = json.load(json_file)
 
 
@@ -89,11 +89,12 @@ for i in dos_dict:
     end = jid.find('.vasp')
     jid = jid[start:end]
     jid_list.append(jid)
+    print(jid)
     target = np.array(i['predictions'])
     freq = np.linspace(0, 1000, len(target))
     int_DOS.append(pint.integrate_dos(freq, target))
-    S_vib.append(pint.vibrational_entropy(freq, target, 1))
-    Cp.append(pint.heat_capacity(freq, target, 1))
+    S_vib.append(pint.vibrational_entropy(freq, target))
+    Cp.append(pint.heat_capacity(freq, target))
 
 pred_output = {'JID' : jid_list,
           'integrated_DOS' : int_DOS,
@@ -101,7 +102,7 @@ pred_output = {'JID' : jid_list,
           'Cp (J/mol/K)' : Cp}
 df = pd.DataFrame(pred_output)
 
-df.to_csv(run + label + '_thermal_props.csv')
+df.to_csv(run + label + '_thermal_props_scale_2.csv')
 
 
 '''
@@ -138,4 +139,4 @@ df2 = pd.DataFrame(MAE)
 
 df_tot = pd.concat([df1, df2], axis=1)
 
-df_tot.to_csv('mae_' + run + '_thermal_props.csv')
+df_tot.to_csv('mae_' + run + '_thermal_props_scale_2.csv')

pdos_integration.py
@@ -61,22 +61,29 @@ def transform_normalized_dos(dft_3d, norm_dos : dict, dos_label = 'target'):
         end = jid.find('.vasp')
         jid = jid[start:end]
         match = next(i for i in dft_3d if i["jid"] == jid)
+        scale = get_natoms_from_db_entry(match)
         freq = np.arange(0, 1000, 5)
         s = Spectrum(x= freq, y=match['pdos_elast'])
         hist, bins = histo_making(1000, 0, 5, s)
         max_intensity = np.max(hist)
         #max_intensity = np.max(match['pdos_elast'])
-        norm_dos[n][dos_label] = np.array(norm_dos[n][dos_label]) * max_intensity
+        norm_dos[n][dos_label] = np.array(norm_dos[n][dos_label]) * max_intensity * scale
     return norm_dos
 
 
-def get_nmol_from_db_entry(p):
-    atoms = Atoms(lattice_mat = p['atoms']['lattice_mat'],\
-                  coords = p['atoms']['coords'], elements = p['atoms']['elements'])
-    form_unit = atoms.composition.reduce()
-    red_atoms = sum([v for v in form_unit[0].values()])  
-    ratio = atoms.num_atoms / red_atoms
-    return red_atoms
+def get_natoms_from_db_entry(p):
+    atoms = Atoms.from_dict(p['atoms'])
+    num_atoms = atoms.num_atoms
+    try:
+        spg = Spacegroup3D(atoms)
+        cvn_atoms = spg.conventional_standard_structure.num_atoms
+    except:
+        cvn_atoms = num_atoms
+    formula = atoms.composition.reduce()
+    form_atoms = sum([v for v in formula[0].values()])
+    #scale = formula[1]
+    scale = (num_atoms / cvn_atoms) * form_atoms
+    return scale
     
     
 
@@ -87,7 +94,7 @@ def integrate_dos(omega, dos):
 
 
 
-def vibrational_entropy(omega, dos, natoms, T = 300):
+def vibrational_entropy(omega, dos, T = 300):
     #Convert inv. cm to eV
     omega = omega * icm_to_eV
     dos = dos / icm_to_eV
@@ -95,18 +102,18 @@ def vibrational_entropy(omega, dos, natoms, T = 300):
     n = 1 / (np.exp(x[1:]) - 1)
     S_vib = kB  * ((n + 1) * np.log(n + 1) + n * np.log(n)) * dos[1:]
     S_vib = np.insert(S_vib, 0, 0)
-    return np.trapz(S_vib, omega) * e * Na * natoms
+    return np.trapz(S_vib, omega) * e * Na
 
 
-def heat_capacity(omega, dos, natoms, T = 300):
+def heat_capacity(omega, dos, T = 300):
     #Convert inv. cm to eV
-    omega = omega * icm_to_eV / natoms
-    dos = dos / icm_to_eV * natoms
+    omega = omega * icm_to_eV
+    dos = dos / icm_to_eV
     x= (omega) / (kB * T)
     #Drop omega = 0 term?
     Cp = kB * x[1:]**2 * (np.exp(x[1:]) / (np.exp(x[1:]) - 1)**2) * dos[1:] #removed factor of 3
     Cp = np.insert(Cp, 0, 0)
-    return np.trapz(Cp, omega) * e * Na * natoms #multiply by #atoms in formula unit??
+    return np.trapz(Cp, omega) * e * Na #multiply by #atoms in formula unit??
     
     
     
@@ -114,7 +121,7 @@ if __name__ == '__main__':
     dft_3d = jdata("edos_pdos")
     max_samples = 10
     
-    jid = 'JVASP-19985'
+    jid = 'JVASP-9430'
     
     x = []
     for i in dft_3d:
@@ -130,13 +137,13 @@ if __name__ == '__main__':
     
     jid = p["jid"]
     
-    n_mols = get_nmol_from_db_entry(p)
+    scale = get_scale_quantity_from_db_entry(p)
     jid_list.append(jid)
     target = np.array(p['pdos_elast'])
     freq = np.linspace(0, 1000, len(target))
     int_DOS.append(integrate_dos(freq, target))
-    S_vib.append(vibrational_entropy(freq, target, n_mols, T = 1000))
-    Cp.append(heat_capacity(freq, target, n_mols, T = 1000))
+    S_vib.append(vibrational_entropy(freq, target, T = 1000))
+    Cp.append(heat_capacity(freq, target, T = 1000))
     
     output = {'JID' : jid_list,
               'integrated_DOS' : int_DOS,